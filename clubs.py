636ad4067e00be2288e80cebebe456a5b9b1bab6
Ryan Buckner <ryanbuckner@gmail.com>
2018-10-15 15:36:35 -0400

    Added new file clubs.py

---
+++ clubs.py
@@ -0,0 +1,41 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+import os
+
+# Default container for returns to API Gateway
+# you will need to add a 'body' key with a JSON
+# string containing the intended response
+retVal = {"isBase64Encoded": False, "statusCode": 200,"headers": {"Access-Control-Allow-Origin": '*', "Access-Control-Allow-Credentials": True}}
+table = os.environ['ClubTableName']
+
+def get_clubs(event, context):
+    '''get_clubs is a Lambda Function used to return all players
+
+        Inputs: 
+        Outputs:
+            JSON formated string containing
+            - message: function outcome
+            - success: True/False if the insert succeeded
+            - clubs: all clubs in a single dict
+    '''
+    dynamodb = boto3.client('dynamodb')
+
+    try:
+        print('Attempting to scan ' + table)
+        response = dynamodb.scan(
+            TableName = table
+        )
+    except Exception as e:
+        print('Scan failed')
+        print(e)
+        
+        retVal['body'] = json.dumps({'message': 'Unable to search table', 'success': False, 'clubs': {}})
+        return retVal
+    else:
+        print("Clubs Returned")
+        clubs = response['Items']
+
+        retVal['body'] = json.dumps({'message': 'Unable to search table', 'success': False, 'clubs': clubs})
+        return retVal
